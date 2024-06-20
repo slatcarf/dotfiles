f6640bf776c25dd208c009d1290b57e96cedc002
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2024-06-21 01:07:08 +0200

    fix: close polybars before launching

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,13 +2,14 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+#polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+ killall -q polybar
+ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log 
 polybar --config=~/.config/polybar/config.ini example
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -r example 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
