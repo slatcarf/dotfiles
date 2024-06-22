fd3458c62f59268b1146472111717f454cc45ee6
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2024-06-22 02:54:15 +0200

    fix: auto yes, source bashrc after fnm install

--- init.sh
+++ init.sh
@@ -61,6 +61,7 @@ install_fnm() {
   else
     # Install fnm using the recommended installation script
     curl -fsSL https://fnm.vercel.app/install | bash
+    . "$HOME/.bashrc"
     fnm install 20
     fnm use 20
   fi
@@ -88,9 +89,9 @@ install_vscode() {
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
     rm -f packages.microsoft.gpg
 
-    sudo apt install apt-transport-https
+    sudo apt install -y apt-transport-https
     sudo apt update
-    sudo apt install code
+    sudo apt install -y code
 
     echo "Visual Studio Code installation complete."
   fi
